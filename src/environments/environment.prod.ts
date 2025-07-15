1a14def00eeefb30a9e5894ced2e117d9fa81958
Fady-Alfred <factaguib@gmail.com>
2025-07-15 03:46:26 +0300

    Google OAuth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url.com/api',
+  apiUrl: 'https://e-commerce-seven-phi-59.vercel.app/api',
   googleClientId: '907639453242-l1bp62r9ef1dtsgq106erbmqk07mmvcq.apps.googleusercontent.com'
 };
