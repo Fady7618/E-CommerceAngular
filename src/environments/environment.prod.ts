d03e1437afe8422667c253e022d2e554a18bf6b1
Fady-Alfred <factaguib@gmail.com>
2025-07-15 17:47:15 +0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://e-commerce-seven-phi-59.vercel.app/api',
+  apiUrl: 'https://192.168.1.12/api',
   googleClientId: '907639453242-l1bp62r9ef1dtsgq106erbmqk07mmvcq.apps.googleusercontent.com'
 };
