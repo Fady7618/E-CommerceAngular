18db70a3d34859b347ae774e18be6ba3121b4b90
Fady-Alfred <factaguib@gmail.com>
2025-07-19 19:46:18 +0300

    Debugging

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable, of, BehaviorSubject } from "rxjs";
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
