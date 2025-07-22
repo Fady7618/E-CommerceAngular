561a0a02a5d2ca6bdd456e6b5a93ad6488894c18
Fady-Alfred <factaguib@gmail.com>
2025-07-22 16:49:22 +0300

    Image Optimization

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 // Load environment variables
 dotenv.config();
@@ -44,6 +45,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
 
 // Initialize Passport
 require('./config/passport-setup')();
